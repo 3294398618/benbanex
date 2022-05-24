c94cf2b87b6d0c5c6e5e0175c11ea38e8bc3c86b
3294398618 <94789493+3294398618@users.noreply.github.com>
2022-05-24 22:59:33 +0800

    Add files via upload

---
+++ ex2.py
@@ -0,0 +1,9 @@
+#a comment,this is so you can read your program later
+#anyting after the #is igonred by python
+
+print("I could have code like this.") #and the comm afte is igonred 
+
+#you can also use a comment to "disable" or comment out code :
+#print ("this won't tun.") 
+
+print ("this will run") 
