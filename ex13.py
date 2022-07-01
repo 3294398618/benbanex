47f0d7c8b71860e1faf909e4182c3acaa924c3df
3294398618 <94789493+3294398618@users.noreply.github.com>
2022-07-01 11:51:27 +0800

    Add files via upload

---
+++ ex13.py
@@ -0,0 +1,8 @@
+from sys import argv
+# read the WYSS section for how to run this
+script, first, second, third = argv    #逗号后面要空格
+
+print("the script is called:", script)
+print("your first variable is:", first)#同上
+print("your second variable is:", second)
+print("your third variable is:", third)
