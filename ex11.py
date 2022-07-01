d237635171aa5b4231ccd3e435642950ff9ec21f
3294398618 <94789493+3294398618@users.noreply.github.com>
2022-07-01 11:50:50 +0800

    Add files via upload

---
+++ ex11.py
@@ -0,0 +1,8 @@
+print("how old are you?",end =' ')11
+age = input()
+print("how tall are you ?",end = ' ')
+height = input()#括号没打
+print("how much do you weight?",end = ' ')
+weight = input()
+
+print(f"so, you'are{age} old,{height} tall and {weight} heavy.")
