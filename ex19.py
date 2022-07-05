23c9636022f94e7f4623e38eb2f52b9dc1e7d56d
3294398618 <94789493+3294398618@users.noreply.github.com>
2022-07-05 20:54:31 +0800

    Add files via upload

---
+++ ex19.py
@@ -0,0 +1,8 @@
+def cheese_and_crackers(cheess_count, boxes_of_crackers):
+    print(f"you have {cheese_count} cheese!")
+    print(f"you have {boxes_of_crackers} boxes of cracker!")
+    print("man that's enough for aparty!")
+    print("get a blanket.\n")
+
+
+
